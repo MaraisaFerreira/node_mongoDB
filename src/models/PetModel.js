53e862a0137cc104629a97fe22f4c6d846bd0f86
maraisaferreira <ferreira.maraisacristina@gmail.com>
2022-04-21 16:46:20 -0300

    alt. pet model fields

--- src/models/PetModel.js
+++ src/models/PetModel.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 const PetSchema = new mongoose.Schema({
+	name: { type: String },
 	kind: { type: String, required: true },
-	breed: { type: String, required: true },
 	age: { type: Number, required: true },
 	color: { type: String, required: true },
-	adopted: { type: Boolean, required: true },
+	url_img: String,
 });
 
 const PetModel = mongoose.model('Pets', PetSchema);
