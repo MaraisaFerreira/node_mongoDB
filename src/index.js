450db616847e154fbb335f8405837c7c2e80e913
maraisaferreira <ferreira.maraisacristina@gmail.com>
2022-04-21 12:06:28 -0300

    mongoDB connected

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
+
+const mongoose = require('mongoose');
+
+mongoose
+	.connect(process.env.URL_CONNECTION)
+	.then(() => {
+		console.log('Connected on db...');
+		app.emit('db_connection_started');
+	})
+	.catch((err) => console.log(err));
 
 const routes = require('./routes');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
-app.listen(3000, () => {
-	console.log('http://localhost:3000');
-	console.log('Server running on port 3000...');
+app.on('db_connection_started', () => {
+	app.listen(3000, () => {
+		console.log('http://localhost:3000');
+		console.log('Server running on port 3000...');
+	});
 });
