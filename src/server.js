7022a0757149e12cb92ead4f1e9878dbc391d82c
maraisaferreira <ferreira.maraisacristina@gmail.com>
2022-04-22 10:09:33 -0300

    save session

--- src/server.js
+++ src/server.js
@@ -13,9 +13,26 @@ mongoose
 	})
 	.catch((err) => console.log(err));
 
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+
 const routes = require('./routes');
 
+const sessionOpt = session({
+	secret: process.env.SECRET,
+	resave: false,
+	saveUninitialized: false,
+	store: MongoStore.create({
+		mongoUrl: process.env.URL_CONNECTION,
+	}),
+	cookie: {
+		maxAge: 1000 * 60 * 60 * 24 * 7,
+		httpOnly: true,
+	},
+});
+
 app.use(express.urlencoded({ extended: true }));
+app.use(sessionOpt);
 app.use(routes);
 
 app.on('db_connection_started', () => {
